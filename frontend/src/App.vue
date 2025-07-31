10802aa95ed1a7b40bfa4021c3dc1f39f0374b56
juanmarcos69 <cosajohnmark69@gmail.com>
2025-07-31 15:59:13 +0800

    up

---
+++ frontend/src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+const message = ref ('Task');
+const newTask = ref ("");
+
+function formSubmitted() {
+console.log(newTask.value);
+
+}
+</script>
+
+<template>
+  <main> 
+    <h1>{{ message }}</h1>
+    <form @submit.prevent="formSubmitted">
+      <label>
+      New task
+      <input v-model="newTask" name="newTask">
+      </label>
+      <div class=" button-container">
+       <button>add</button>
+      </div>
+    </form>
+
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
+}
+.button-container{
+  display: flex;
+  justify-content: end;
+}
+</style>
